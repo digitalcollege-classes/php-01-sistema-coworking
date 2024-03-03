f78fb3d1b913b582a64cc0063f09b21f1d6c4824
Paulo Victor <dev.paulovictor@gmail.com>
2024-03-03 18:17:32 -0300

    Remove importacao do bootstrap duplicada

--- src/views/usuario/listar.php
+++ src/views/usuario/listar.php
@@ -1,7 +1,6 @@
 <?php
     use App\Core\DatabaseConnection;
 ?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 <section class="container mt-3">
     <h2>Lista Usuários</h2>
