b48febc00c59c3639d4b61ac6d3ef18a771bb102
Matheus Ferreira <ferreira@MacBook-Air-de-Matheus.local>
2024-03-10 13:56:36 -0300

    Atribui ao botão de Ver Salas na Homepage a pagina de Listar Salas

--- src/views/home.php
+++ src/views/home.php
@@ -8,7 +8,7 @@
 
     <h2>Salas Disponíveis</h2>
     <p>Explore nossas salas e escolha o espaço perfeito para o seu trabalho.</p>
-    <a href="/salas-disponiveis" class="btn btn-primary w-50 mx-auto" style="font-size: 1.2em; padding: 10px 20px;">Ver Salas</a>
+    <a href="/salas/listar" class="btn btn-primary w-50 mx-auto" style="font-size: 1.2em; padding: 10px 20px;">Ver Salas</a>
 
     <img src="https://i0.wp.com/catagua.com.br/wp-content/uploads/2021/10/o-que-e-e-quais-as-vantagens-do-espaco-coworking.jpg?resize=1024%2C683&ssl=1" alt="Imagem Ilustrativa" style="width: 100%; max-width: 500px; margin: 0 auto;">
 </section>
